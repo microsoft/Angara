5260091dcd0a95a3223d927e969f240b74fc6a27
Dmitry Voytsekhovskiy <v-dmvo@microsoft.com>
2016-02-17 18:04:59 +0300

    Configuring release configurations

--- src/Angara.Html/AssemblyInfo.fs
+++ src/Angara.Html/AssemblyInfo.fs
@@ -8,36 +8,18 @@ module internal Const =
     [<Literal>]
     let Version = "0.1.0"
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [<assembly: AssemblyTitle("Angara.Html")>]
 [<assembly: AssemblyDescription("Dynamically generates an HTML page that displays an object.")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("Microsoft Research")>]
 [<assembly: AssemblyProduct("Angara")>]
-[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyCopyright("Copyright © 2016 Microsoft Research")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("714b3368-671c-4452-837b-f487a8e027a4")>]
 
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
 [<assembly: AssemblyVersion(Const.Version + ".0")>]
 [<assembly: AssemblyFileVersion(Const.Version + ".0")>]
 
--- src/Angara.Reinstate/AssemblyInfo.fs
+++ src/Angara.Reinstate/AssemblyInfo.fs
@@ -6,26 +6,18 @@ open System.Runtime.InteropServices
 
 module internal Const =
     [<Literal>]
-    let Version = "1.0.0"
+    let Version = "0.1.0"
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [<assembly: AssemblyTitle("Angara.Reinstate")>]
-[<assembly: AssemblyDescription("Library that reinstates computation results either from the storage or recomputing a value from the function")>]
+[<assembly: AssemblyDescription("Library that reinstates computation results either from the storage or recomputing a value from the function.")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("Microsoft Research")>]
-[<assembly: AssemblyProduct("Angara.Reinstate")>]
+[<assembly: AssemblyProduct("Angara")>]
 [<assembly: AssemblyCopyright("Copyright © Microsoft Research 2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("d3c4dedf-431d-4816-b07c-4be07fdd687d")>]
 
 [<assembly: AssemblyVersion(Const.Version + ".0")>]
