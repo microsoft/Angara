102a1565879a141d793f5465d7e15917e6585acd
Dmitry Voytsekhovskiy <v-dmvo@microsoft.com>
2016-03-21 12:19:11 +0300

    Preparing tests for Angara.html

---
+++ src/Angara.Html.Tests/Angara.Html.Tests.fs
@@ -0,0 +1,8 @@
+﻿module Angara.Html.Tests
+
+open NUnit.Framework
+
+//[<Test; Category("CI")>]
+//let Bug_StackOverflowWhenSave() =
+//    let ch_data = Table.Load("1.ChAreaApproximated.csv")
+//    Html.Save "Observations.html" ch_data
